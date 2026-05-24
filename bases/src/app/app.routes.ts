c6fdb1ef827b06b6e1aa43f1c517cea75e83cc98
Deivy Rojas <deivy.rojas@puntored.com>
2026-05-24 11:22:53 -0500

    Section 4 Complete

--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {CounterPageComponent} from './pages/counter/counter-page.component';
 import {CalculatorPageComponent} from './pages/calculator/calculator-page.component';
+import { HeroPageComponent } from './pages/hero/hero-page.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'calculator',
     component: CalculatorPageComponent
+  },
+  {
+    path: 'hero',
+    component: HeroPageComponent
   }
 ];
--- bases/src/app/pages/counter/counter-page.component.ts
+++ bases/src/app/pages/counter/counter-page.component.ts
@@ -1,21 +1,33 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component , signal} from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
   styles:`
-  button{
-    padding: 5px;
-    margin: 5px 10px;
-    width: 75px;
-  }
+    button{
+      padding: 5px;
+      margin: 5px 10px;
+      width: 75px;
+    }
   `,
+  //changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class CounterPageComponent{
   counter = 10;
+  counterSignal = signal(10)
+
+  constructor(){
+    setInterval(() => {
+      // this.counter +=1;
+      // this.counterSignal.update((v) => v + 1);
+      this.increaseBy(1);
+      console.log('Tick');
+    }, 2000);
+  }
 
   increaseBy(value: number){
     this.counter += value;
+    this.counterSignal.update( current => current + value)
   }
 
   decreaseBy(value: number) {
@@ -23,7 +35,8 @@ export class CounterPageComponent{
   }
 
   resetCounter(value: number){
-    this.counter = 10
+    this.counter = 0;
+    this.counterSignal.set(0);
   }
 
 }
